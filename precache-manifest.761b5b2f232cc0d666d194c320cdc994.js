02c2c8d0832777b6ff0ebb1e73668530f3d2d19d
Shcherbaniuk Serhii <shcherbaniuk.s@hexide-digital.com>
2020-04-24 17:25:15 +0300

    Updates

---
+++ precache-manifest.761b5b2f232cc0d666d194c320cdc994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d367702d480ae82a243a7391720a0411",
+    "url": "/diploma/index.html"
+  },
+  {
+    "revision": "231444e2110c6e0b79ca",
+    "url": "/diploma/static/css/main.f9f86165.chunk.css"
+  },
+  {
+    "revision": "00c3c5b386a9d34fb460",
+    "url": "/diploma/static/js/2.0d8d4343.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/diploma/static/js/2.0d8d4343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231444e2110c6e0b79ca",
+    "url": "/diploma/static/js/main.36e127e0.chunk.js"
+  },
+  {
+    "revision": "71f06b37bfc14b88ae8b",
+    "url": "/diploma/static/js/runtime-main.217b6534.js"
+  }
+]);
